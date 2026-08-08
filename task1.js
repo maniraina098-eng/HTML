d5c1480383dbec73770f403b36f9213aa5df7c04
maniraina098-eng <maniraina098@gmail.com>
2026-08-08 23:36:15 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,266 @@
+
+// TASK 1 
+console.log("TASK 1");
+
+const collegeName = "AVS College";
+let studentName = "Manikandan";
+var studentAge = 28;
+
+console.log(collegeName);
+console.log(studentName);
+console.log(studentAge);
+
+
+// TASK 2 
+console.log("TASK 2");
+
+let price = 25000;
+price = 27000;
+console.log(price);
+
+
+// TASK 3 
+console.log("TASK 3");
+
+const company = "Thestackly";
+console.log(company);
+
+
+// TASK 4 
+console.log("TASK 4");
+
+console.log("Login Successful");
+console.warn("Password is Weak");
+console.error("Network Error");
+
+
+// TASK 5 
+console.log("TASK 5");
+
+let myName = "Manikandan";
+let myAge = 28;
+let isDeveloper = true;
+let uninitialized;
+
+console.log(myName, typeof myName);
+console.log(myAge, typeof myAge);
+console.log(isDeveloper, typeof isDeveloper);
+console.log(uninitialized, typeof uninitialized);
+
+
+// TASK 6 
+console.log("TASK 6");
+
+let cart = [ "Laptop", "Mouse", "Keyboard", "Headset", "Monitor"];
+
+console.log(cart[0]);              
+console.log(cart[2]);              
+console.log(cart[cart.length - 1]); 
+
+
+// TASK 7 
+console.log("TASK 7");
+
+let movies = [ "Ethirneechal", "Amaran", "Maaveran", "Velaikaran", "Seiyon", "Hero"];
+
+console.log(movies[1]);                 
+console.log(movies[4]);                 
+console.log(movies[movies.length - 1]); 
+
+
+// TASK 8 
+console.log("TASK 8");
+
+let employee = {
+    name: "Manikandan",
+    age: 28,
+    department: "IT",
+    skills: ["JavaScript", "HTML", "CSS","JAVA","C++"],
+    salary: 40000
+};
+
+console.log(employee.name);
+console.log(employee.department);
+console.log(employee.skills[0]);
+console.log(employee.salary);
+
+
+// TASK 9 
+console.log("TASK 9");
+
+let product = {
+    productName: "Mobile",
+    brand: "Apple",
+    price: 60000,
+    colors: ["Black", "Red", "White"]
+};
+
+console.log(product.brand);
+console.log(product.price);
+console.log(product.colors[1]);
+
+
+// TASK 10 
+console.log("TASK 10");
+
+let Mani = 28;
+let Mk = 25;
+
+console.log("Addition:", Mani + Mk);
+console.log("Subtraction:", Mani - Mk);
+console.log("Multiplication:", Mani * Mk);
+console.log("Division:", Mani / Mk);
+console.log("Modulus:", Mani % Mk);
+console.log("Exponent:",Mani ** Mk);
+
+
+// TASK 11 
+console.log("TASK 11");
+
+let a = 5;
+
+a++;
+console.log(a); 
+++a;
+console.log(a); 
+a--;
+console.log(a); 
+--a;
+console.log(a); 
+
+
+// TASK 12 
+console.log("TASK 12");
+
+let a12 = 10;
+let b12 = a12++;
+let c12 = ++b12;
+
+console.log(a12); 
+console.log(b12); 
+console.log(c12); 
+
+
+// TASK 13
+console.log("TASK 13");
+
+let x = 7;
+let y = --x;
+let z = y++;
+
+console.log(x); 
+console.log(y); 
+console.log(z); 
+
+
+// TASK 14 
+console.log("TASK 14");
+
+let school = {
+    schoolName: "Government Higher Secondary School",
+    principal: "Mr.Ashok",
+    totalStudents: 1000,
+    classes: ["9th","10th","11th","12th"]
+};
+
+console.log(school.schoolName);
+console.log(school.principal);
+console.log(school.classes[3]);
+
+
+// TASK 15 
+console.log("TASK 15");
+
+let groceries = ["Eggs","Bread","Salt","Sugar","Tea","Oil","Milk",];
+
+console.log(groceries[0]);                 
+console.log(groceries[3]);                 
+console.log(groceries[6]); 
+
+
+// TASK 16 - User Profile
+console.log("TASK 16");
+
+let username = "Maniraina";
+let email = "manikandan098@thestackly.com";
+let age = 28;
+let isPremium = true;
+
+console.log(username, typeof username);
+console.log(email, typeof email);
+console.log(age, typeof age);
+console.log(isPremium, typeof isPremium);
+
+
+// TASK 17 
+console.log("TASK 17");
+
+var varValue = 10;
+varValue = 15;       //  Reassignment allowed
+var varValue = 20;   //  Redeclaration allowed
+
+console.log(varValue);
+
+let letValue = 10;
+letValue = 15;       // Reassignment allowed
+
+// let letValue = 20; // Redeclaration not allowed
+console.log(letValue);
+
+const constValue = 10;
+
+// constValue = 15;   // Reassignment not allowed
+// const constValue = 20; // Redeclaration not allowed
+
+console.log(constValue);
+
+
+// TASK 18 
+console.log("TASK 18");
+
+let tamil = 85;
+let english = 90;
+let maths = 95;
+let total = tamil + english + maths;
+let average = total / 3;
+
+console.log("Total:", total);
+console.log("Average:", average);
+
+
+// TASK 19 
+console.log("TASK 19"); 
+
+let Company = {
+    Employees: ["Mani", "sri", "sathish", "manoj"],
+    Location: "Omalur",
+    FoundedYear: 2020,
+    CEO: "Nishanth"
+};
+
+console.log(Company.CEO);
+console.log(Company.Location);
+console.log(Company.Employees[0]);
+
+
+// TASK 20
+console.log("TASK 20");
+
+let profile = {
+    name: "Manikandan",
+    city: "Salem",
+    hobbies: ["Cricket", "sleeping", "cooking"],
+    age: 28
+};
+
+console.log(profile.name);
+console.log(profile.hobbies[1]);
+console.log(profile.age);
+
+
+
+
+
+      
+
+
